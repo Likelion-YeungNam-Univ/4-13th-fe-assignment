b2ed902c34e3e481d318f299d79b17025aad3d43
nyeonseok <123443488+nyeonseok@users.noreply.github.com>
2025-04-13 01:46:54 +0900

    calculate함수 구현, 이벤트 리스너 등록 완료

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // 요소 선택
 const historyWrap = document.querySelector(".history-wrap");
 const result = document.querySelector(".result");
+const numBtns = document.querySelectorAll(".num-btn"); //0 ~ 9, C, <- 버튼
+const calcBts = document.querySelectorAll(".calc-btn"); // +, -, *, /, = 버튼
 
 // 변수 선언
 let formula = ""; // 수식
@@ -10,6 +12,8 @@ let historyList = []; // 계산 기록
 // 기록 업데이트
 const updateHistory = () => {
   // 1. historyWrap 내 HTML 초기화 ✅
+  historyWrap.innerHTML = "";
+
   // 2. historyWrap 내 계산 기록(historyList) 요소들 추가 ✅
 };
 
@@ -22,7 +26,28 @@ const deleteHistory = (index) => {
 // 계산 함수
 const calculate = (e) => {
   // 1. 클릭된 값 받아오기 ✅
+  // console.log(e);
+  const input = e.target.innerText;
+
   // 2. 클릭된 값에 따라 동작 구현(=, C, ←, 나머지) ✅
+  if (input === "==") {
+    if (formula === "") return;
+
+    try {
+      resultNum = eval(formula);
+      resultNum = Math.round(resultNum * 100) / 100;
+    } catch {}
+  } else if (input === "C") {
+    formula = "";
+    result.innerText = "";
+  } else if (input === "←") {
+    formula = formula.slice(0, -1);
+    // console.log(formula);
+    result.innerText = formula;
+  } else {
+    formula += input;
+    result.innerText = formula;
+  }
   // 추가 정보
   // HTML 내의 &#8592; 는 화살표이며, JavaScript에선 ← 를 사용
   // formula 내의 수식을 계산할 때는 resultNum = eval(formula) 를 사용
@@ -30,5 +55,11 @@ const calculate = (e) => {
 };
 
 // .num-bt 이벤트 리스너 등록 ✅
+numBtns.forEach((btn) => {
+  btn.addEventListener("click", calculate);
+});
 
 // .calc-btn 이벤트 리스너 등록 ✅
+calcBts.forEach((btn) => {
+  btn.addEventListener("click", calculate);
+});
