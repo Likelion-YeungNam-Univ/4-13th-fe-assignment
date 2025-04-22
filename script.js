242ea546fcbd72d65e5a73b1b38d23de138811d2
gyeongsangseaman <sadx302@gmail.com>
2025-04-23 00:41:35 +0900

    기록 업데이트 함수, C버튼 수정 및 소수점 2자리 출력 구현

--- script.js
+++ script.js
@@ -10,33 +10,36 @@ let historyList = []; // 계산 기록
 // 기록 업데이트
 const updateHistory = () => {
   // 1. historyWrap 내 HTML 초기화 ✅
+  historyWrap.innerHTML = ""; // innerHTML을 초기화 하면 historyWrap 내의 요소를 초기화 -> 만들었던 <li>태그들이 제거됨
+
   // 2. historyWrap 내 계산 기록(historyList) 요소들 추가 ✅
-  //
-  // 이게 위에 출력하는 거임. historyWrap에다가.
-  // = 눌렀을 때 계산하고 바로 List로 넣고 이 함수에서는 List 값을 Wrap으로 옮기는 건가 ?
-  const addHistory = document.createElement("li");
-  addHistory.innerText = historyList[historyList.length - 1];
+  // historyList의 각 요소에 대해서
+  historyList.forEach((history) => {
+    // <li>태그를 생성, 태그에 해당 요소의 내용을 넣음, class 지정
+    const addHistory = document.createElement("li");
+    addHistory.innerText = history;
+    addHistory.className = "added-history";
 
-  const deleteBtn = document.createElement("button");
-  deleteBtn.innerText = "X";
+    // 삭제 버튼을 추가
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "X";
+    deleteBtn.className = "delete-button";
 
-  deleteBtn.addEventListener("click", () => {
-    historyWrap.removeChild(addHistory);
+    // 삭제 버튼을 클릭하면 deleteHistory함수를 호출, 이때 해당 요소의 index를 인자로 넘겨서 실행
+    deleteBtn.addEventListener("click", () => {
+      deleteHistory(historyList.indexOf(history)); // .indexOf()는 배열에서 요소의 위치를 찾거나 존재 여부를 확인할 때 유용
+    });
+
+    // 이렇게 만들어진 게 historyWrap에 출력되도록
+    addHistory.appendChild(deleteBtn);
+    historyWrap.appendChild(addHistory);
   });
-
-  addHistory.appendChild(deleteBtn);
-  historyWrap.appendChild(addHistory);
 };
 
-const deleteHistory = () => {
+const deleteHistory = (index) => {
   // historyList에서 해당 인덱스 요소 제거 ✅
-  // 이거 5개까지만 보이고 초과될 때마다 지우는 건가 ?
-  historyList.splice(0, 1); // historyList 배열에서 0번 인덱스 제거
-
-  const temp = document.querySelector("li"); // 가장 먼저 선택되는 <li>태그(= 가장 먼저 추가됐던 <li>태그)를 temp에 지정
-
-  historyWrap.removeChild(temp); // temp에 지정된 <li>태그 제거
-  // updateHistory();
+  historyList.splice(index, 1); // historyList 배열에서 "index부터 1개 제거" 라는 뜻 (= 해당 index 제거)
+  updateHistory();
 };
 
 // 계산 함수
@@ -50,24 +53,25 @@ const calculate = (e) => {
   // 계산 결과가 소수일 경우 소수점 두 번째 자리까지만 계산
   //
   // 이거 혹시 밑에 이벤트 리스너 안에 함수를 여기다 다 옮기고 .addEventListener("click", calculate()); 하면 됨 ?
+  // 이렇게 하면 안 됨. 삭제 버튼 이벤트 리스너에서. 화살표 함수로 호출하는 방식이 맞다고. .addEventListener("click", () => {calculate()});
   // nBtn이랑 cBtn이랑 무관하게 btn으로 써도 되나 ?
-  // 된다면 -> else 내용이 중복이라 축약된다는 이점 있음
+  // 된다면 else 내용이 중복이긴 해서 축약된다는 이점이 있기는 한데
 };
 
 // .num-btn 이벤트 리스너 등록 ✅
-const numBtn = document.querySelectorAll(".num-btn");
+const numBtn = document.querySelectorAll(".num-btn"); // num-btn 클래스 NodeList로
 
-// forEach()를 사용해서 NodeList 각 버튼마다 처리하도록, nBtn은 현재 클릭된 버튼
+// forEach()를 사용해서 NodeList 각 버튼마다 처리하도록, nBtn은 현재 클릭된 num-btn 버튼
 numBtn.forEach((nBtn) => {
   nBtn.addEventListener("click", () => {
     // 만약 누른 버튼이 C 라면
-    // 누르면 기록까지 전부 다 없어져야 함
+    // 누르면 기록까지 전부 다 없어져야 함 -> 모든 값 초기화 후 최신화
     if (nBtn.innerText == "C") {
       formula = "";
       resultNum = 0;
       historyList = [];
       result.innerText = formula;
-      // updateHistory();
+      updateHistory();
     }
     // 또는 누른 버튼이 ← 라면
     else if (nBtn.innerText == "←") {
@@ -85,18 +89,20 @@ numBtn.forEach((nBtn) => {
 // .calc-btn 이벤트 리스너 등록 ✅
 const calcBtn = document.querySelectorAll(".calc-btn"); // calc-btn 클래스 NodeList로
 
-// forEach()를 사용해서 NodeList 각 버튼마다 처리하도록, cBtn은 현재 클릭된 버튼
+// forEach()를 사용해서 NodeList 각 버튼마다 처리하도록, cBtn은 현재 클릭된 calc-btn 버튼
 calcBtn.forEach((cBtn) => {
   cBtn.addEventListener("click", () => {
     // 만약 누른 버튼이 = 이라면
     if (cBtn.innerText == "=") {
-      resultNum = eval(formula); // 수식(formula)을 계산한 값을 계산 결과(resultNum)에 넣고
-      result.innerText = resultNum; // <div>태그에 넣어서 출력
+      resultNum = Number(eval(formula).toFixed(2)); // 수식(formula)을 계산하는데, toFixed(2)로 소수점 2번째 자리까지 출력. parseFloat로 무조건적으로 소수점 2자리까지 출력되는 문제를 해결. 계산된 값을 계산 결과(resultNum)에 넣고
+      result.innerText = resultNum; // 계산 결과를 <div>태그에 넣어서 출력
       historyList.push(`${formula} = ${resultNum}`); // 수식과 결과를 historyList 배열 끝에 문자열로 추가
+      // 만약 기록이 5개를 초과한다면
       if (historyList.length == 6) {
-        deleteHistory();
+        // 첫 번째 인덱스를 삭제
+        historyList.splice(0, 1);
       }
-      updateHistory();
+      updateHistory(); // 기록이 추가될 때마다 최신화
       formula = resultNum; // 수식(formula)의 값을 계산 결과로 초기화
     }
     // 아니라면
@@ -108,7 +114,5 @@ calcBtn.forEach((cBtn) => {
 });
 
 // 다음에 켰을 때
-// C 버튼 기능 다시
-// 이벤트 리스너에서 실행되는 함수 합치기(?)
-// 소수점 2자리까지만 출력
-// historyWrap 출력에서 X버튼으로 지워도 historyList에는 남아서 출력이 5개가 아닌데 계속 최신화되는 문제
+// 이벤트 리스너에서 실행되는 함수를 계산 함수 파트로 옮기기
+// 소수점 2자리까지만 출력 -> 조건문으로 결과가 정수면 그냥 출력, 소수점이 3자리 이상으로 생기면 2자리까지만 출력되도록
