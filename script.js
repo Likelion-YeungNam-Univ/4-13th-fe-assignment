9f7379ce619c9c41188581ba5d241806ffec122e
DESKTOP-OMUF54L\류승현 <nex721@naver.com>
2025-04-29 23:36:43 +0900

    script.js 구현

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // 요소 선택
 const historyWrap = document.querySelector(".history-wrap");
 const result = document.querySelector(".result");
+const num_btn = document.querySelectorAll(".num-btn");
+const calc_btn = document.querySelectorAll(".calc-btn");
 
 // 변수 선언
 let formula = ""; // 수식
@@ -10,25 +12,62 @@ let historyList = []; // 계산 기록
 // 기록 업데이트
 const updateHistory = () => {
   // 1. historyWrap 내 HTML 초기화 ✅
+  historyWrap.innerHTML = "";
+
   // 2. historyWrap 내 계산 기록(historyList) 요소들 추가 ✅
+  historyList.forEach((item, index) => {
+    const li = document.createElement("li");
+
+    const span = document.createElement("span");
+    span.innerText = item;
+
+    const del_btn = document.createElement("button");
+    del_btn.innerText = "X";
+    del_btn.addEventListener("click", () => deleteHistory(index));
+
+    li.appendChild(span);
+    li.appendChild(del_btn);
+    historyWrap.appendChild(li);
+  });
 };
 
 const deleteHistory = (index) => {
   // historyList에서 해당 인덱스 요소 제거 ✅
-
+  historyList.splice(index, 1);
   updateHistory();
 };
 
 // 계산 함수
 const calculate = (e) => {
   // 1. 클릭된 값 받아오기 ✅
+  const inputValue = e.target.innerText;
+
   // 2. 클릭된 값에 따라 동작 구현(=, C, ←, 나머지) ✅
-  // 추가 정보
-  // HTML 내의 &#8592; 는 화살표이며, JavaScript에선 ← 를 사용
-  // formula 내의 수식을 계산할 때는 resultNum = eval(formula) 를 사용
-  // 계산 결과가 소수일 경우 소수점 두 번째 자리까지만 계산
+  if (inputValue == "=") {
+    resultNum = eval(formula);
+    resultNum = resultNum.toFixed(2);
+    historyList.push(formula + " = " + resultNum);
+    result.innerText = resultNum;
+    formula = "";
+    updateHistory();
+  } else if (inputValue == "C") {
+    formula = "";
+    result.innerText = resultNum;
+  } else if (inputValue == "←") {
+    formula = formula.slice(0, -1);
+    result.innerText = resultNum;
+  } else {
+    formula += inputValue;
+    result.innerText = resultNum;
+  }
 };
 
-// .num-bt 이벤트 리스너 등록 ✅
+// .num-btn 이벤트 리스너 등록 ✅
+num_btn.forEach(function (btn) {
+  btn.addEventListener("click", calculate);
+});
 
 // .calc-btn 이벤트 리스너 등록 ✅
+calc_btn.forEach(function (btn) {
+  btn.addEventListener("click", calculate);
+});
